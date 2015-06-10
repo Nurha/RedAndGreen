d24e2e25efc349f7cc72b9c78c0178308718ab1c
coder <nurha@projectcodex.co>
2015-06-10 14:12:27 +0200

    task 3 is done

---
+++ task3.js
@@ -0,0 +1,6 @@
+	function myFunction() {
+    	document.getElementById("square1").style.backgroundColor = 'red';
+    }
+    function myFunction1(){
+    	document.getElementById("square1").style.backgroundColor = 'green';	
+    }
